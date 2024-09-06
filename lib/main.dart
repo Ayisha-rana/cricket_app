5d0e119f5e84b27cd38dcfc197a1756d5ddf16d5
Ayisha <ayisharana137@gmail.com>
2024-09-06 19:57:45 +0530

    maiiin

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:cricket_app/screen/homePage.dart';
-import 'package:cricket_app/screen/playerpage.dart';
-import 'package:cricket_app/screen/privacypolicy.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Crickaet App',
+      title: 'Cricket App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
